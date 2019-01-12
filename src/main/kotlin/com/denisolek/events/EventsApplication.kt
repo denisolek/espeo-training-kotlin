1c78692a1424ad4d0f38e4f236d4be287d31476f
denisolek <denis.olek@rsqtechnologies.com>
2019-01-12 12:24:20 +0100

    init

---
+++ src/main/kotlin/com/denisolek/events/EventsApplication.kt
@@ -0,0 +1,12 @@
+package com.denisolek.events
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EventsApplication
+
+fun main(args: Array<String>) {
+    runApplication<EventsApplication>(*args)
+}
+
